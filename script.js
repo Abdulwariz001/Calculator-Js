8fa8a6f7a6f3bbbf7f986b94f6a0c2790919f922
Azeez Wariz <warizazeez05@gmail.com>
2022-11-25 15:14:02 +0100

    mycalculator project

---
+++ script.js
@@ -0,0 +1,89 @@
+let input1 =document.getElementById("input1")
+
+function one(){
+    document.getElementById("input1").value += "1"
+}
+
+function two(){
+    document.getElementById("input1").value += "2"
+}
+
+
+function three(){
+    document.getElementById("input1").value += "3"
+}
+
+function four(){
+    document.getElementById("input1").value += "4"
+}
+
+
+function five(){
+    document.getElementById("input1").value += "5"
+}
+
+function six(){
+    document.getElementById("input1").value += "6"
+}
+
+
+function seven(){
+    document.getElementById("input1").value += "7"
+}
+
+function eight(){
+    document.getElementById("input1").value += "8"
+}
+
+
+function nine(){
+    document.getElementById("input1").value += "9"
+}
+
+function zero(){
+    document.getElementById("input1").value += "0"
+}
+
+
+function add(){
+    document.getElementById("input1").value += "+"
+}
+
+function sub(){
+    document.getElementById("input1").value += "-"
+}
+
+function doublezero(){
+    document.getElementById("input1").value += "00"
+}
+
+function divide(){
+    document.getElementById("input1").value += "/"
+}
+
+
+function star(){
+    document.getElementById("input1").value += "*"
+}
+
+function dot(){
+    document.getElementById("input1").value += "."
+}
+
+function clearD(){
+    document.getElementById("input1").value = " "
+}
+
+function dash(){
+    document.getElementById("input1").value = eval(document.getElementById("input1").value)
+}
+
+function Ac(){
+    let input1 =document.getElementById("input1")
+    input1.value = input1.value.slice(0, -1)
+}
+
+
+
+
+
